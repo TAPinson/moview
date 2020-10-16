6b8897422738fa0a1eeedeec6d55ca983d66e9d1
Travis Pinson <travispinson@gmail.com>
2020-10-16 09:32:27 -0500

    updated with approved wireframe

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          Welcome to Moview!
-       
- 
+          <h1>Welcome to Moview!</h1>
     </div>
   );
 }
 
+export const Kennel = () => (
+  <>
+      <h2>Nashville Kennels</h2>
+      <small>Loving care when you're not there.</small>
+      <address>
+          <div>Visit Us at the Nashville North Location</div>
+          <div>500 Puppy Way</div>
+      </address>
+  </>
+)
 export default App;
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/">Home</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/queue">Our Queue</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/likes">My Likes</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/watched">My Watched</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/account">My Account</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/logout">Logout</Link>
+            </li>
+        </ul>
+    )
+}
