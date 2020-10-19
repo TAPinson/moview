0ac9f6476494c6dc28a1671d2930f355c6cd9e77
Travis Pinson <travispinson@gmail.com>
2020-10-19 11:46:07 -0500

    fixed bug causing finder to crash when movie poster not provided

--- src/components/movie/MovieFinder.js
+++ src/components/movie/MovieFinder.js
@@ -6,16 +6,9 @@ import "./Movie.css"
 export const MovieFinder = () => {
     const { movies, getMovies, getRandomMovies, addSelection } = useContext(MovieContext)
     const [ movie , setMovie] = useState({})
-    
-    //console.log(movies)
-    
-    //useEffect - reach out to the world for something
     useEffect(() => {
-		//console.log("MovieList: useEffect - getMovies")
         getRandomMovies()
-		
     }, [])
-
     let results = []
     const movieResults = movies.results
     if (movieResults) {
@@ -23,41 +16,43 @@ export const MovieFinder = () => {
             results.push(each)
         })
         let token = Math.floor(Math.random() * (20 - 0 + 1)) + 0; // returns a random integer from 1 to 20
-        console.log(token)
         let movie = results[token]
-  
-        const imgURL = `http://image.tmdb.org/t/p/w300//${movie.poster_path}`
-        //console.log(results[0])
-        const userId = parseInt(localStorage.getItem("user"))
-        return (
-            <>
-            <section className="finderContainer">
-                <div className="finderViewer">
-                    <h1>{movie.title}</h1>
-                    <img className="moviePoster" src={imgURL} alt="movie poster"></img>
-                    <div className="movie__overview"><strong>Overview:</strong> {movie.overview}</div>
-                    <button onClick={() => {
-                        const selection = {
-                            userId,
-                            watched: false,
-                            tmdbObject: movie
-                        }
-                        addSelection(selection)
-                        .then(() =>{
+        if (movie.poster_path) {
+            const imgURL = `http://image.tmdb.org/t/p/w300//${movie.poster_path}`
+            const userId = parseInt(localStorage.getItem("user"))
+            return (
+                <>
+                <section className="finderContainer">
+                    <div className="finderViewer">
+                        <h1>{movie.title}</h1>
+                        <img className="moviePoster" src={imgURL} alt="movie poster"></img>
+                        <div className="movie__overview"><strong>Overview:</strong> {movie.overview}</div>
+                        <button onClick={() => {
+                            const selection = {
+                                userId,
+                                watched: false,
+                                tmdbObject: movie
+                            }
+                            addSelection(selection)
+                            .then(() =>{
+                                getRandomMovies()
+                            })
+                            }}>Like
+                        </button>
+                        <button onClick={() => {
                             getRandomMovies()
-                        })
-                        //getRandomMovies()
-                        }}>Like
-                    </button>
-                    <button onClick={() => {
-                        getRandomMovies()
-                        }}>Pass
-                    </button>
-                    
-                </div>
-            </section>
-            </>
-        )
+                            }}>Pass
+                        </button>
+                    </div>
+                </section>
+                </>
+            )
+
+        }
+        
+        
+        
+        
 
 
     } 
