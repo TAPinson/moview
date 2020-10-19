aaf1806d571b816dc552b16aa40c46d2fbd30db6
Travis Pinson <travispinson@gmail.com>
2020-10-19 08:51:16 -0500

    Random movie populating in finder on page load

--- src/components/movie/MovieFinder.js
+++ src/components/movie/MovieFinder.js
@@ -1,13 +1,48 @@
-import React from "react"
+import React, {useContext, useEffect} from "react"
+import { MovieContext } from "./MovieProvider"
 
 import "./Movie.css"
 
 export const MovieFinder = () => {
-  
+    const { movies, getMovies, getRandomMovies } = useContext(MovieContext)
+    //console.log(movies)
+    
+    //useEffect - reach out to the world for something
+    useEffect(() => {
+		//console.log("MovieList: useEffect - getMovies")
+        getRandomMovies()
+		
+    }, [])
+
+    let results = []
+    const movieResults = movies.results
+    if (movieResults) {
+        movieResults.map((each) => {
+            results.push(each)
+        })
+        const movie = results[0]
+        const imgURL = `http://image.tmdb.org/t/p/w300//${movie.poster_path}`
+        //console.log(results[0])
+        return (
+            <>
+            <section className="finderContainer">
+                <div className="finderViewer">
+                    <h1>{movie.title}</h1>
+                    <img classname="moviePoster" src={imgURL} alt="movie poster"></img>
+                    <div className="movie__overview"><strong>Overview:</strong> {movie.overview}</div>
+                    
+                </div>
+            </section>
+            </>
+        )
+
+
+    } 
+    
 
     return (
         <>
-        <section class="finderContainer">
+        <section className="finderContainer">
             <div className="finderViewer">
                 <h1>Movie Display</h1>
             </div>
