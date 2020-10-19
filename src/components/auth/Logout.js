b5778caaaccfadf89946661b2ac375c40232dd87
Travis Pinson <travispinson@gmail.com>
2020-10-19 11:35:28 -0500

    added functionality to logout button, also redirects to login

--- src/components/auth/Logout.js
+++ src/components/auth/Logout.js
@@ -1,8 +1,12 @@
 import React from "react"
+import { useHistory, useParams } from 'react-router-dom';
 
 
 
 export const Logout = () => {
+    const history = useHistory()
+    localStorage.removeItem("user")
+    history.push("/")
   
 
     return (
