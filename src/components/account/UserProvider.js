b97f718f0d6883bbf01d579ea4bbc7b8d66fd052
Travis Pinson <travispinson@gmail.com>
2020-10-20 13:46:54 -0500

    implemented ability for user to assign their queue partner

--- src/components/account/UserProvider.js
+++ src/components/account/UserProvider.js
@@ -5,6 +5,7 @@ export const UserContext = createContext()
 export const UserProvider = (props) => {
     const loggedId = localStorage.getItem("user")
     const [user, setUser] = useState([])
+    const [users, setUsers] = useState([])
 
     const getUser = () => {
         // Fetch popular movies from 2019
@@ -14,16 +15,32 @@ export const UserProvider = (props) => {
             let user = parsedUser[0]
             setUser(user)
         })
-        //.then(setUser)
-        
     }
 
+    const getUsers = () => {
+        return fetch(`http://localhost:8088/users`)
+        .then(res => res.json())
+        .then(parsedUser => {
+            let users = parsedUser
+            setUsers(users)
+        })
+    }
 
+    const updateUser = user => {
+        return fetch(`http://localhost:8088/users/${user.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+        .then(setUser(user))
+    }
 
     // Add needed functionality to context
     return (
         <UserContext.Provider value={{
-            user, getUser
+            user, getUser, users, getUsers, updateUser
         }}>
             {props.children}
         </UserContext.Provider>
--- src/components/account/ViewerAccount.js
+++ src/components/account/ViewerAccount.js
@@ -5,45 +5,103 @@ import "./account.css"
 
 
 export const ViewerAccount = () => {
-    const { user, getUser } = useContext(UserContext)
+    const { user, getUser, getUsers, users, updateUser } = useContext(UserContext)
     
-
-    //useEffect - reach out to the world for something
     useEffect(() => {
-		//console.log("MovieList: useEffect - getMovies")
         getUser()
+        .then(getUsers)
         
     }, [])
 
-    
     let searchTerms;
     const searchTermFinder = (event) => {
         searchTerms = event.target.value
-        console.log(searchTerms)
     }
-    
-    
-    
 
-    return (
-        <>
-        <div className="userBox">
-            <h1>This will be where you edit account details</h1>
-                <div>Username: {user.username}</div>
-                <div>Email: {user.email}</div>
-                <form>
-                    <fieldset>
-                        <input type="text"
-                                id="partnerInput"
-                                name="partnerId"
-                                required
-                                className="form-control"
-                                placeholder={user.partnerId}
-                                onChange={searchTermFinder} 
-                        />
-                    </fieldset>
-                </form>
+    const constructUser = (partner) => {
+        const pairedPartner = users.filter((each) => {
+            return each.username === partner
+        })
+        const partnerId = pairedPartner[0].id
+        // Construct the updated user object to PUT
+        const userWithPartner = {
+            username: user.username,
+            password: user.password,
+            email: user.email,
+            partnerId: partnerId,
+            id: user.id
+        }
+        updateUser(userWithPartner)
+    }
 
-        </div>
-        </>
-    )}
+    //Use users partnerId to retrieve the whole object of the partner
+    const partnerFinder = users.filter((each) =>{
+        return each.id === user.partnerId
+    })
+    // An array is returned from filter, so we only need the 0th element of the returned array
+    const partnerUser = partnerFinder[0]
+
+    // If a valied partner has been found, render the following way
+    if (partnerUser !== undefined) {
+        return (
+            <>
+            <div className="userBox">
+                <h1>This will be where you edit account details</h1>
+                    <div>Username: {user.username}</div>
+                    <div>Email: {user.email}</div>
+                    <div>Partner: {user.partnerId}</div>
+                    <form>
+                        <fieldset>
+                            <input type="text"
+                                    id="partnerInput"
+                                    name="partnerId"
+                                    required
+                                    className="form-control"
+                                    defaultValue={partnerUser.username}
+                                    onChange={searchTermFinder} 
+                            />
+                        </fieldset>
+                        <button type="submit"
+                                        onClick={evt => {
+                                            evt.preventDefault() // Prevent browser from submitting the form
+                                            constructUser(searchTerms)
+                                        }}
+                                        className="partnerSaveBtn">
+                                         Save
+                        </button>
+                    </form>
+            </div>
+            </>
+        )
+    } else {
+        return (
+            <>
+            <div className="userBox">
+                <h1>This will be where you edit account details</h1>
+                    <div>Username: {user.username}</div>
+                    <div>Email: {user.email}</div>
+                    <div>Partner: {user.partnerId}</div>
+                    <form>
+                        <fieldset>
+                            <input type="text"
+                                    id="partnerInput"
+                                    name="partnerId"
+                                    required
+                                    className="form-control"
+                                    onChange={searchTermFinder} 
+                            />
+                        </fieldset>
+                        <button type="submit"
+                                        onClick={evt => {
+                                            evt.preventDefault() // Prevent browser from submitting the form
+                                            constructUser(searchTerms)
+                                        }}
+                                        className="partnerSaveBtn">
+                                         Save
+                        </button>
+                    </form>
+            </div>
+            </>
+        )
+    }
+}
