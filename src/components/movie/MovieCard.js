3dac81adfaea58583fe812d36e5659b7f95622ac
Travis Pinson <travispinson@gmail.com>
2020-10-21 15:32:17 -0500

    removed some commented out code

--- src/components/movie/MovieCard.js
+++ src/components/movie/MovieCard.js
@@ -145,44 +145,6 @@ export const QueueCard = ({ movie }) => {
         </section>
 )}
 
-// This is used to display movies already selected by the user and being retrieved from JSON Server
-// export const WatchedCard = ({ movie }) => {
-//     const { deleteSelection, MyLikes, updateSelection } = useContext(MovieContext)
-//     const imgURL = `http://image.tmdb.org/t/p/w185//${movie.tmdbObject.poster_path}`
-//     useEffect(() => {
-//         MyLikes()
-//     }, [])
-//     return (
-//         <section className="movieBox">
-//             <h3 className="movie__name">{movie.tmdbObject.title}</h3>
-//             <div className="movie__overview">{movie.tmdbObject.overview}</div>
-//             <div className="movie__release"><strong>Released:</strong> {movie.tmdbObject.release_date}</div>
-//             <img className="moviePoster" src={imgURL} alt="movie poster"></img>
-//             <div>
-//                 <button className="likedDelete" onClick={() => {
-//                     deleteSelection(movie.id)
-//                     .then(() => {
-//                         MyLikes()
-//                     })
-//                     }}>Delete
-//                 </button>
-//                 <button className="likedMark"onClick={() => {
-//                     const watchedMovie = {
-//                         userId: loggedInUser,
-//                         watched: false,
-//                         tmdbObject: movie.tmdbObject,
-//                         id: movie.id
-//                     }
-//                     updateSelection(watchedMovie)
-//                     .then(() => {
-//                         MyLikes()
-//                     })
-//                     }}>Watch Again
-//                 </button>
-//             </div>
-//         </section>
-// )}
-
 export const WatchedCard = ({ movie }) => {
     const { deleteSelection, MyLikes, updateSelection, addComment } = useContext(MovieContext)
     const { users, getUsers } = useContext(UserContext)
