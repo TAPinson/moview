2c04ef0187d8bb8a6ae6e972badc0632de81a736
Travis Pinson <travispinson@gmail.com>
2020-10-21 10:06:14 -0500

    css change / font change

--- src/components/movie/MovieCard.js
+++ src/components/movie/MovieCard.js
@@ -13,7 +13,7 @@ export const MovieBrowse = ({ movie }) => {
     return (
         <section className="movieBox">
             <h3 className="movie__name">{movie.title}</h3>
-            <div className="movie__overview"><strong>Overview:</strong> {movie.overview}</div>
+            <div className="movie__overview">{movie.overview}</div>
             <div className="movie__release"><strong>Released:</strong> {movie.release_date}</div>
             <img className="moviePoster" src={imgURL} alt="movie poster"></img>
             <div>
