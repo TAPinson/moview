6180db61c8970b0a5919e06896b9321cc70b4b3c
Travis Pinson <travispinson@gmail.com>
2020-10-23 15:57:04 -0500

    single movie by genre buttons now rendering when movie was searched

--- src/components/movie/MovieFinder.js
+++ src/components/movie/MovieFinder.js
@@ -156,6 +156,33 @@ export const MovieFinder = () => {
                             </button>
                         </form>
                     </div>
+                    <div className="finderGenreButtons">
+                        <button className="comedySearchBtn" onClick={() => {
+                            searchByGenre("28")
+                            }
+                            }>Show An Action
+                        </button>
+                        <button className="comedySearchBtn" onClick={() => {
+                            searchByGenre("35")
+                            }
+                            }>Show A Comedy
+                        </button>
+                        <button className="comedySearchBtn" onClick={() => {
+                            searchByGenre("99")
+                            }
+                            }>Show A Documentary
+                        </button>
+                        <button className="comedySearchBtn" onClick={() => {
+                            searchByGenre("18")
+                            }
+                            }>Show A Drama
+                        </button>
+                        <button className="comedySearchBtn" onClick={() => {
+                            searchByGenre("14")
+                            }
+                            }>Show A Fantasy
+                        </button>
+                    </div>
                 </section>
                 </>
             )
