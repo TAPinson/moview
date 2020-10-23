e33f0104c14d1b970b37285656ddf33ecc88404d
Travis Pinson <travispinson@gmail.com>
2020-10-23 15:30:32 -0500

    refactored moviequeue with fallback messages

--- src/components/movie/MovieQueue.js
+++ src/components/movie/MovieQueue.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from "react"
 import {UserContext} from '../account/UserProvider'
 import { MovieContext } from "./MovieProvider"
-import { MovieCard } from "./Views/MovieCard"
 import { QueueCard } from './Views/QueueCard'
 import "./Movie.css"
 
@@ -14,18 +13,30 @@ export const MovieQueue = () => {
         getUsers()
         .then(MyLikes)
     }, [])
+
+    // Get the ID of the logged in user
     const loggedInUserId = parseInt(localStorage.getItem("user"))
+    // Get an array of logged in users selections
     const myMovies = movies.filter((movie) => {
         return movie.userId === loggedInUserId && movie.watched === false
     })
+    // Get the full object of the logged in user
     const userObject = users.find((user) => {
         return user.id === loggedInUserId
     })
-    if (userObject !== undefined){
-        const partnerId = userObject.partnerId
+    // Get the full object of logged in users partner ID
+    const partnerObject = users.find((user) => {
+        return user.id ===userObject.partnerId
+    })
+    // If a partner has been selected, continue
+    if (partnerObject !== undefined){
+        // get the ID of the users partner
+        const partnerId = partnerObject.id
+        // Find matches by iterating movies and returning those that have been made by users partner AND are marked unwatched
         const partnerMovies = movies.filter((movie) => {
             return movie.userId === partnerId && movie.watched === false
         })
+        // This is the function that attaches all comments to one of the movies, since there are 2 selection in play
         const queueMovies = myMovies.map((myMovie) => {
             for (const qMovie of partnerMovies) {
                 if (qMovie.tmdbObject.id === myMovie.tmdbObject.id)  {
@@ -42,31 +53,34 @@ export const MovieQueue = () => {
                 }
             }
         })
-        return (	
-            <>
-            <div className="movies">
-            {   //console.log(queueMovies),
-                queueMovies.map(movie => {
-                    if (movie !== undefined) {
-                        return <QueueCard key={movie.id} movie={movie} />
-                    }
-                })
-            }
-            </div>
-            </>
-        )
+        // Check to ensure that there are matched movies and if so, map them
+        if (queueMovies.length > 0) {
+            return (	
+                <>
+                <div className="movies">
+                {   queueMovies.map(movie => {
+                        if (movie !== undefined) {
+                            return <QueueCard key={movie.id} movie={movie} />
+                        }
+                    })
+                }
+                </div>
+                </>
+            )
+        }
+        // If there are not matched movies, notify the user 
+        else {
+            return (
+                <h1 className="queueFallback">No Matches Yet! Select more movies!</h1>
+            )
+        }
     } 
-    if (userObject === undefined){
+    // If a partner has not been selected, notify user they will need a partner
+    if (partnerObject === undefined){
         return (	
             <>
-            <h1>No Movies</h1>
-            <div className="movies">
-            {   
-                myMovies.map(movie => {
-                    return <MovieCard key={movie.id} movie={movie} />
-                })
-            }
-            </div>
+            <h1 className="queueFallback">You'll have to select a partner to build a queue!</h1>
+            
             </>
         )
     }
