9ad843b89f84aa26a71781d1572aa03eea7dea0d
Travis Pinson <travispinson@gmail.com>
2020-10-27 09:25:49 -0500

    removed passwords for accounts, minor styling on login / register area

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -6,46 +6,34 @@ import "./Login.css"
 
 export const Login = props => {
     const username = useRef()
-    const password = useRef()
     const existDialog = useRef()
-    const passwordDialog = useRef()
     const history = useHistory()
-
     const existingUserCheck = () => {
         return fetch(`http://localhost:8088/users?username=${username.current.value}`)
             .then(_ => _.json())
             .then(user => user.length ? user[0] : false)
     }
-
     const handleLogin = (e) => {
         e.preventDefault()
-
         existingUserCheck()
             .then(exists => {
-                if (exists && exists.password === password.current.value) {
+                if (exists ) {
                     localStorage.setItem("user", exists.id)
                     history.push("/")
-                } else if (exists && exists.password !== password.current.value) {
-                    passwordDialog.current.showModal()
                 } else if (!exists) {
                     existDialog.current.showModal()
                 }
             })
     }
-
     return (
         <main className="container--login">
             <dialog className="dialog dialog--auth" ref={existDialog}>
                 <div>User does not exist</div>
                 <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
             </dialog>
-            <dialog className="dialog dialog--password" ref={passwordDialog}>
-                <div>Password does not match</div>
-                <button className="button--close" onClick={e => passwordDialog.current.close()}>Close</button>
-            </dialog>
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Welcome to Moview!</h1>
+                    <h1 className="loginWelcome">Welcome to Moview!</h1>
                     <h2>Please sign in</h2>
                     <fieldset>
                         <label htmlFor="inputUsername"> Username </label>
@@ -56,24 +44,17 @@ export const Login = props => {
                             required autoFocus />
                     </fieldset>
                     <fieldset>
-                        <label htmlFor="inputPassword"> Password </label>
-                        <input ref={password} type="password"
-                            id="password"
-                            className="form-control"
-                            placeholder="Password"
-                            required />
-                    </fieldset>
-                    <fieldset>
                         <button type="submit">
                             Sign in
                         </button>
                     </fieldset>
+                    <section className="link--register">
+                        <Link to="/register">Not a member yet?</Link>
+                    </section>
                 </form>
+                
             </section>
-            <section className="link--register">
-                <Link to="/register">Not a member yet?</Link>
-            </section>
+            
         </main>
     )
 }
-
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -4,59 +4,39 @@ import "./Login.css"
 
 export const Register = (props) => {
     const email = useRef()
-    const password = useRef()
-    const verifyPassword = useRef()
-    const passwordDialog = useRef()
     const history = useHistory()
     const username = useRef()
-
     const existingUserCheck = () => {
         return fetch(`http://localhost:8088/users?email=${email.current.value}`)
             .then(_ => _.json())
             .then(user => !!user.length)
     }
-
     const handleRegister = (e) => {
-        
         e.preventDefault()
-        
-
-        if (password.current.value === verifyPassword.current.value) {
             existingUserCheck()
-                .then(() => {
-                    fetch("http://localhost:8088/users", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            username: username.current.value,
-                            password: password.current.value,
-                            email: email.current.value,
-                            partnerId: 0
-                        })
+            .then(() => {
+                fetch("http://localhost:8088/users", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        username: username.current.value,
+                        email: email.current.value,
+                        partnerId: 0
                     })
-                        .then(_ => _.json())
-                        .then(createdUser => {
-                            if (createdUser.hasOwnProperty("id")) {
-                                localStorage.setItem("user", createdUser.id)
-                                history.push("/")
-                            }
-                        })
                 })
-        } else {
-            passwordDialog.current.showModal()
-        }
+                    .then(_ => _.json())
+                    .then(createdUser => {
+                        if (createdUser.hasOwnProperty("id")) {
+                            localStorage.setItem("user", createdUser.id)
+                            history.push("/")
+                        }
+                    })
+            })
     }
-
     return (
         <main style={{ textAlign: "center" }}>
-
-            <dialog className="dialog dialog--password" ref={passwordDialog}>
-                <div>Passwords do not match</div>
-                <button className="button--close" onClick={e => passwordDialog.current.close()}>Close</button>
-            </dialog>
-
             <form className="form--login" onSubmit={handleRegister}>
                 <h1 className="h3 mb-3 font-weight-normal">Register for Moview</h1>
                 <fieldset>
@@ -73,22 +53,6 @@ export const Register = (props) => {
                         name="email"
                         className="form-control"
                         placeholder="Email address"
-                        required />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="inputPassword"> Password </label>
-                    <input ref={password} type="password"
-                        name="password"
-                        className="form-control"
-                        placeholder="Password"
-                        required />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="verifyPassword"> Verify Password </label>
-                    <input ref={verifyPassword} type="password"
-                        name="verifyPassword"
-                        className="form-control"
-                        placeholder="Verify password"
                         required />
                 </fieldset>
                 <fieldset>
